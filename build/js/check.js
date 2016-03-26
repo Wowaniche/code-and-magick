0ab6b991aaf07a7da886a00e426c290dc1370b41
Wowaniche <dobl-king@mail.ru>
2016-03-26 22:36:25 +0300

    module1-task2
    Исправил check.js, но есть вопросы.

--- build/js/check.js
+++ build/js/check.js
@@ -1,25 +1,30 @@
-var arr = [a, b];
-var messageString = function getMessage(a, b) {
-
-	if (a === true) {
-		return 'Я попал в' + b ;
+var a;
+var b;
+var arr; 
+function getMessage() {
+ 	
+	if (a == true) {
+	  return 'Я попал в ' + b;
 	} else {
-		return 'Я никуда не попал';
+	  return 'Я никуда не попал';
 	}
 
-	if (isNan(a)) {
-		return 'Я прыгнул на' + a*100 + 'сантиметров';
+	if (isFinite(a)) {
+		return 'Я прыгнул на ' + a*100 + ' сантиметров';
 	}
 
-	if (Array.isArray([a])) {
-		var result = arr.reduce(function(sum, current) {
-  	return sum + current;
-		}, 0);
-	}
+	if (Array.isArray(a)) {
+			var result = a.reduce(function(sum, current) {
+	    return sum + current;
+			}, 0);
+		}
 
-	if (Array.isArray(arr)) {
-		return arr.length[0]*arr.length[1];
+	if (Array.isArray(a) && Array.isArray(b)) {
+		for (i = 0; i < arr.length; i++) {
+			arr = a[i]*b[i];
+		}
+		return arr;	 
 	}
 }
 
-getMessage(a, b);
+getMessage();
