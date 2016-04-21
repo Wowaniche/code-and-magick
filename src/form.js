982859b267759cab576aed2a351f08b5c64f7fec
Wowaniche <dobl-king@mail.ru>
2016-04-22 02:43:22 +0300

    module3-task3
    Исправления

--- src/form.js
+++ src/form.js
@@ -8,17 +8,18 @@
 
 (function() {
   var cookies = require('browser-cookies');
-
   var formContainer = document.querySelector('.overlay-container');
   var formOpenButton = document.querySelector('.reviews-controls-new');
   var formCloseButton = document.querySelector('.review-form-close');
   var formFieldName = document.querySelector('.review-form-field-name');
+  formFieldName.value = cookies.get('name') || '';
   var formReviewMarkOne = document.querySelector('.review-mark-label-1');
   var formReviewMarkTwo = document.querySelector('.review-mark-label-2');
   var formReviewMarkThree = document.querySelector('.review-mark-label-3');
   var formReviewMarkFour = document.querySelector('.review-mark-label-4');
   var formReviewMarkFive = document.querySelector('.review-mark-label-5');
   var formFieldText = document.querySelector('.review-form-field-text');
+  formFieldText.value = cookies.get('text') || '';
   var formReviewFieldsName = document.querySelector('.review-fields-name');
   var formReviewFieldsText = document.querySelector('.review-fields-text');
   var formReviewSubmit = document.querySelector('.review-submit');
@@ -59,18 +60,18 @@
   function checkRemainedRequiredFields() {
     var name = formFieldName.value;
     var text = formFieldText.value;
-    var isNameValid = name != 0;
-    var isTextValid = text != 0;
+    var isNameValid = name === '';
+    var isTextValid = text === '';
 
-    if (isNameValid) {
+    if (!isNameValid) {
       formReviewFieldsName.setAttribute('hidden', true);
     }
 
-    if (isTextValid) {
+    if (!isTextValid) {
       formReviewFieldsText.setAttribute('hidden', true);
     }
 
-    if (!isNameValid && !isTextValid) {
+    if (isNameValid && isTextValid) {
       formReviewFieldsName.removeAttribute('hidden', true);
       formReviewFieldsText.removeAttribute('hidden', true);
     }
@@ -79,16 +80,14 @@
   function addDisabledOnSubmit() {
     var name = formFieldName.value;
     var text = formFieldText.value;
-    var isNameInValid = name === '';
-    var isTextInValid = text === '';
 
-    if (isNameInValid) {
+    if (name === '') {
       formReviewSubmit.setAttribute('disabled', true);
     } else {
       formReviewSubmit.removeAttribute('disabled', true);
     }
 
-    if (isTextInValid) {
+    if (text === '') {
       formReviewMarkOne.onclick = function() {
         formReviewSubmit.setAttribute('disabled', true);
       };
@@ -145,7 +144,7 @@
   }
 
   function calcDayToTheBirthday() {
-    var now      = new Date();
+    var now = new Date();
     var currYear = now.getFullYear();
     var birthday = new Date(currYear, 9, 9);
 
@@ -153,21 +152,27 @@
   }
 
   function setCookie() {
-    var text = cookies.get('text') || formFieldText.value;
-    var name = cookies.get('name') || formFieldName.value;
     var expiresTime = Date.now() + calcDayToTheBirthday();
 
-    cookies.set('text', text, {
+    cookies.set('name', formFieldName.value, {
       expires: expiresTime
     });
-    cookies.set('name', name, {
+
+    cookies.set('text', formFieldText.value, {
       expires: expiresTime
     });
   }
 
   formReviewSubmit.onsubmit = function() {
-    isValidForm();
     setCookie();
     this.submit();
+
+    if(!isValidForm()) {
+      return false;
+    } else {
+      return true;
+    }
   };
 })();
+
+
