893af3fe046d07b6b5bd63259e4bfe2a79ccdf75
Wowaniche <dobl-king@mail.ru>
2016-05-06 04:41:11 +0300

    module9-task1
    module9-task1

--- src/reviews-object.js
+++ src/reviews-object.js
@@ -1,32 +1,35 @@
 'use strict';
 
-var reviewQuizAnswerYes = document.querySelector('.review-quiz-answer-yes');
-var reviewQuizAnswerNo = document.querySelector('.review-quiz-answer-yes');
-
 var getReviewElement = require('./get-review-element.js');
 
 var Review = function(data, container) {
   this.data = data;
   this.element = getReviewElement.getReviewElement(this.data, container);
+  this.reviewQuizAnswerYes = this.element.querySelector('.review-quiz-answer-yes');
+  this.reviewQuizAnswerNo = this.element.querySelector('.review-quiz-answer-no');
 
-  this.onQuizYesClick = function(evt) {
-    reviewQuizAnswerYes.classList.add('.review-quiz-answer-active');
-  };
+  this.onQuizYesClick = this.onQuizYesClick.bind(this);
+  this.onQuizNoClick = this.onQuizNoClick.bind(this);
 
-  this.onQuizNoClick = function(evt) {
-    reviewQuizAnswerNo.classList.add('.review-quiz-answer-active');
-  };
-
-  this.remove = function() {
-    this.element.removeEventListener('click', this.onQuizYesClick);
-    this.element.removeEventListener('click', this.onQuizNoClick);
-    this.element.parentNode.removeChild(this.element);
-  };
-
-  this.element.addEventListener('click', this.onQuizYesClick);
-  this.element.addEventListener('click', this.onQuizNoClick);
+  this.reviewQuizAnswerYes.addEventListener('click', this.onQuizYesClick);
+  this.reviewQuizAnswerNo.addEventListener('click', this.onQuizNoClick);
   container.appendChild(this.element);
+};
 
+Review.prototype.onQuizYesClick = function() {
+  this.reviewQuizAnswerYes.classList.add('review-quiz-answer-active');
+  this.reviewQuizAnswerNo.classList.remove('review-quiz-answer-active');
+};
+
+Review.prototype.onQuizNoClick = function() {
+  this.reviewQuizAnswerNo.classList.add('review-quiz-answer-active');
+  this.reviewQuizAnswerYes.classList.remove('review-quiz-answer-active');
+};
+
+Review.prototype.remove = function() {
+  this.reviewQuizAnswerYes.removeEventListener('click', this.onQuizYesClick);
+  this.reviewQuizAnswerNo.removeEventListener('click', this.onQuizNoClick);
+  this.element.parentNode.removeChild(this.element);
 };
 
 module.exports = Review;
